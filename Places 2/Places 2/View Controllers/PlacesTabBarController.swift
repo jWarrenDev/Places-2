9287dc8b9c6aff9f9c69387c7e328f4f71f5a58c
jWarrenDev <Jerrick.Warren@gmail.com>
2018-09-06 10:20:06 -0700

    subclass all viewcontrollers

---
+++ Places 2/Places 2/View Controllers/PlacesTabBarController.swift
@@ -0,0 +1,19 @@
+//
+//  PlacesTabBarController.swift
+//  Places 2
+//
+//  Created by Welinkton on 9/6/18.
+//  Copyright © 2018 Jerrick Warren. All rights reserved.
+//
+
+import UIKit
+
+class PlacesTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
